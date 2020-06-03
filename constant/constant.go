f3ec5b4e7d0a0b6dfc78df7fa6e4c9147d19ff38
L. M. Riza Rizky <riza.kepler@gmail.com>
2020-06-03 20:15:56 +0800

    Added algorithm package

--- constant/constant.go
+++ constant/constant.go
@@ -1,13 +1,9 @@
 package constant
 
-/*
-ITEMCOUNT is the number of items,
-that is tradable by the player.
-*/
+// ITEMCOUNT is the number of items,
+// that is tradable by the player.
 const ITEMCOUNT int = 4
 
-/*
-REPORTCOUNT is the number of logs,
-that the game can stores.
-*/
+// REPORTCOUNT is the number of logs,
+// that the game can stores.
 const REPORTCOUNT int = 1000000
